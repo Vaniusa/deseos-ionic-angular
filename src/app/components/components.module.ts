315c98c807b5143f0742992f5efe95ecc4774c59
vaniusa <ionsaitan1980@gmail.com>
2019-06-10 22:48:21 +0200

    Componente lista

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {ListasComponent} from './listas/listas.component';
 import {Lista} from '../models/lista.models';
+import {IonicModule} from '@ionic/angular';
 
 @NgModule({
-  declarations: [
-      ListasComponent
-  ],
-  exports: [
-      ListasComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+    declarations: [
+        ListasComponent
+    ],
+    exports: [
+        ListasComponent
+    ],
+    imports: [
+        CommonModule,
+        IonicModule
+    ]
 })
-export class ComponentsModule { }
+export class ComponentsModule {
+}
 
--- src/app/components/listas/listas.component.ts
+++ src/app/components/listas/listas.component.ts
@@ -1,14 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {DeseosService} from '../../services/deseos.service';
+import {Lista} from '../../models/lista.models';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-listas',
-  templateUrl: './listas.component.html',
-  styleUrls: ['./listas.component.scss'],
+    selector: 'app-listas',
+    templateUrl: './listas.component.html',
+    styleUrls: ['./listas.component.scss'],
 })
 export class ListasComponent implements OnInit {
+    @Input() terminada = true;
 
-  constructor() { }
+    constructor(public deseosService: DeseosService,
+                private  router: Router) {
+    }
 
-  ngOnInit() {}
+    ngOnInit() {
+    }
+
+    listaSeleccionada(lista: Lista) {
+        if (this.terminada) {
+            this.router.navigateByUrl(`/tabs/tab2/agregar/${lista.id}`);
+        } else {
+            this.router.navigateByUrl(`/tabs/tab2/agregar/${lista.id}`);
+        }
+    }
 
 }
